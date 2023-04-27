e6da3f06f71a74090ccbbeb450cea8ffaec2b355
gabrielepetralia <gabri.petralia03@gmail.com>
2023-04-27 19:02:49 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vite DC Comics
+
+
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name : 'App'
+}
+</script>
+
+<template>
+  <h1>APP</h1>
+</template>
+
+<style lang="scss" scoped>
+  @use './scss/main.scss';
+</style>
